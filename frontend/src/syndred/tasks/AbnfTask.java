bb93c26fa6772e6470748c5ae1b8f4b47bfedd95
Phil <me@phil.re>
2017-10-24 23:11:23 +0200

    Abnf extends Task

--- frontend/src/syndred/tasks/AbnfTask.java
+++ frontend/src/syndred/tasks/AbnfTask.java
@@ -1,5 +1,14 @@
 package syndred.tasks;
 
-public class AbnfTask {
+public class AbnfTask extends Task {
+
+	public AbnfTask(String gramma) {
+		super(gramma);
+	}
+
+	@Override
+	public void run() {
+		System.out.println("abnf");
+	}
 
 }
